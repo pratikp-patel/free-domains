737af6d8d565cf4117faa0c9f5c3d4d28ce168af
pratikp-patel <pratikp.patel@truestaz.com>
2023-04-21 16:54:36 +0530

    Add .is-an.app.js File

---
+++ domains/pratikp-patel.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'pratikp-patel',
+  owner: {
+    repo: 'https://github.com/pratikp-patel/pratikp-patel.github.io',
+    email: 'pratikp.patel@truestaz.com,
+  },
+  record: {
+    CNAME: 'pratikp-patel.github.io',
+  },
+  proxy: false,
+})
